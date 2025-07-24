fd8119529ebaae9ddaa2057556fab14c2ee42a6d
AYESHA ASGHAR <ayeshh7766@gmail.com>
2025-07-24 22:49:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,267 @@
+const students = {
+  student1: {
+    id: 1,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student2: {
+    id: 2,
+    name: "tayyaba",
+    age: 21,
+    grade: "B+"
+  },
+  student3: {
+    id: 3,
+    name: "alishba",
+    age: 24,
+    grade: "A"
+  },
+  student4: {
+    id: 4,
+    name: "dua",
+    age: 20,
+    grade: "A"
+  },
+  student5: {
+    id: 5,
+    name: "aleena",
+    age: 20,
+    grade: "c"
+  },
+  student6: {
+    id: 6,
+    name: "uroosa",
+    age: 23,
+    grade: "A"
+  },
+  student7: {
+    id: 7,
+    name: "hamna",
+    age: 12,
+    grade: "b"
+  },
+  student8: {
+    id: 8,
+    name: "aqsa",
+    age: 20,
+    grade: "A"
+  },
+  student9: {
+    id: 9,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student10: {
+    id: 10,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student11: {
+    id: 11,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student12: {
+    id: 12,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student13: {
+    id: 1,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student13: {
+    id: 13,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student14: {
+    id: 14,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student15: {
+    id: 15,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student1: {
+    id: 1,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student16: {
+    id: 16,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student17: {
+    id: 1,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student17: {
+    id: 17,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student18: {
+    id: 18,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student19: {
+    id: 19,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student20: {
+    id: 20,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student21: {
+    id: 21,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+  student22: {
+    id: 22,
+    name: "huma",
+    age: 20,
+    grade: "A"
+  },
+   student23: {
+    id: 23,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+   student24: {
+    id: 24,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+   student25: {
+    id: 25,
+    name: "bareera",
+    age: 20,
+    grade: "A"
+  },
+   student26: {
+    id: 26,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+   student27: {
+    id: 27,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+   student28: {
+    id: 28,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+   student29: {
+    id: 29,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+   student30: {
+    id: 30,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+   student31: {
+    id: 31,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+   student32: {
+    id: 32,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+   student32: {
+    id: 32,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+   student33: {
+    id: 33,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+   student34: {
+    id: 34,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+   student35: {
+    id: 35,
+    name: "sadaf",
+    age: 20,
+    grade: "A"
+  },
+   student36: {
+    id: 36,
+    name: "alishba",
+    age: 20,
+    grade: "A"
+  },
+   student37: {
+    id: 37,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+   student38: {
+    id: 38,
+    name: "ayesha",
+    age: 20,
+    grade: "A"
+  },
+   student39: {
+    id: 39,
+    name: "huma",
+    age: 20,
+    grade: "A"
+  },
+ 
+  student40: {
+    id: 40,
+    name: "inza",
+    age: 22,
+    grade: "A-"
+  }
+};
